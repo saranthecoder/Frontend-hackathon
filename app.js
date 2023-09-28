fc291fcb71e3e2756e02a04ae6ba930fe9802187
saranthecoder <115061220+saranthecoder@users.noreply.github.com>
2023-09-28 18:36:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
       //  https://bot-server-pjk8.onrender.com/predict
-        const api = "https://bot-server-pjk8.onrender.com/predict"
+        const api = "https://ai-bot-server-zvs0.onrender.com/predict"
         //'http://127.0.0.1:3000/predict'
         fetch(api, {
             method: 'POST',
